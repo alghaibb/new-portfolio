18a2aa7524277c837da47e2d6d72ff7be95089d5
Moudi <mjaderi97@gmail.com>
2023-10-24 01:11:39 +1100

    Added social icons & more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
@@ -7,7 +9,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-20 flex justify-between items-center px-4 bg-[#3e71ff] text-gray-300'>
+    <div className='fixed w-full h-20 flex justify-between items-center px-4 bg-[#1552ad] text-gray-300'>
       <div>
         <img src={Logo} alt="Logo image" style={{width: '60px'}}/>
       </div>
@@ -27,7 +29,7 @@ const Navbar = () => {
       </div>
       
       {/* mobile menu */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#3e71ff] flex flex-col justify-center items-center'}>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#1552ad] flex flex-col justify-center items-center'}>
         <li className='pt-6 text-4xl'>Home</li>
         <li className='pt-6 text-4xl'>About</li>
         <li className='pt-6 text-4xl'>Skills</li>
@@ -36,9 +38,32 @@ const Navbar = () => {
       </ul>
       
       {/* social icons */}
-      <div className='hidden'></div>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#013cff]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Linkedin <FaLinkedin size={30}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              GitHub <FaGithub size={30}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Linkedin <HiOutlineMail size={30}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">
+              Resume <BsFillPersonLinesFill size={30}/>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
